0c1caa6356ff41365d8a72460aab7745c35e138c
Dogukan Batal <dogukanbatal@gmail.com>
2024-01-08 00:27:10 +0300

    Add mantine librariy

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { Header } from "@/components/Header";
+import { Button } from "@mantine/core";
 
 export default function Home() {
   return (
     <main>
       <Header title="This is the header component" />
+      <Button variant="filled" color="red">
+        Button
+      </Button>
     </main>
   );
 }
